d7ad50ddc62ab3920cc5e8c304bad6a5c3a38645
jpca999 <jpca999@gmail.com>
2017-05-21 21:42:02 -0400

    --

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,7 +3,9 @@ import { Provider } from 'react-redux';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  StatusBar,
+  TextInput,
 } from 'react-native';
 
 
@@ -11,9 +13,14 @@ class Main extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <StatusBar barStyle="light-content"/>
         <View style={styles.topBar}>
           <Text style={styles.title}> To-DO List
           </Text>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input}/>
         </View>
 
         <Text style={styles.welcome}>
@@ -42,12 +49,24 @@ const styles = StyleSheet.create({
   },
   title:{
     color: 'white',
-    fontSize: 20, 
+    fontSize: 20,
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+  inputContainer:{
+    padding: 8,
+    paddingTop: 0,
+    backgroundColor: '#2ecc71'
+  },
+  input:{
+    height: 26,
+    padding: 4,
+    paddingLeft: 8,
+    borderRadius: 8,
+    backgroundColor: 'white'
   },
   instructions: {
     textAlign: 'center',
